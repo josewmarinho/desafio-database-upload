09b4d072f32331fe4e607267a076103c99627779
Jose Wellington <josewellingtonjunior@hotmail.com>
2020-11-09 20:37:35 -0300

    subindo desafio

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    // Busca no banco de dados, Existe? - Deleta
+    const transactionsRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
